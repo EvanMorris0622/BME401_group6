5b1a9713b5928caeb6e1a208c480d0ce44b62722
EvanMorris0622 <54509852+EvanMorris0622@users.noreply.github.com>
2022-06-07 16:51:51 -0500

    update

--- saveData.m
+++ saveData.m
@@ -19,9 +19,12 @@ function saveData(rawdata, sampleddata, rate)
     raw.Properties.VariableNames(1:6) = {'Time (s)', 'X [V]', 'Y [V]', 'Radius [V]', 'Threshold Measured', 'Trigger Pulse'};
     sampled.Properties.VariableNames(1:6) = {'Time (s)', 'X [V]', 'Y [V]', 'Radius [V]', 'Threshold Measured', 'Trigger Pulse'};
 
-    rawfilename = cat(1, "joystick_rawdata_", c(1:6)', ".csv"); %saved file name 
-    sampledfilename = cat(1, "joystick_sampleddata_", c(1:6)', ".csv"); %saved file name 
+    rawfilename = strjoin(cat(1, "joystick_rawdata_", c(1:5)', ".csv")); %saved file name 
+    sampledfilename = strjoin(cat(1, "joystick_sampleddata_", c(1:5)', ".csv")); %saved file name 
 
-    writetable(raw,string(strjoin(rawfilename)));
-    writetable(sampled,string(strjoin(sampledfilename)));
+    raw_path = fullfile([pwd '\Data'], rawfilename);
+    sampled_path = fullfile([pwd '\Data'], sampledfilename); 
+    
+    writetable(raw,raw_path);
+    writetable(sampled,sampled_path);
 end
